2ce4e336c08684a9b7fae318d08d0bc10f0910ca
elgantrica <ricka_msr@hotmail.com>
2020-05-29 18:50:27 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 // Requires 
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
 
 // Inicializar variables
 var app = express();
 
+//Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
 // Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
-    if( err ) throw err;
+    if (err) throw err;
     console.log('Base de datos:\x1b[32m%s\x1b[0m', 'online');
-    
+
 });
 
 // Rutas
-app.get('/', (req, resp, next) => {
-    resp.status(403).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 // Escuchar peticiones
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log('Express server puerto 3000:\x1b[32m%s\x1b[0m', 'online');
-    
+
 });
---
+++ middlewares/autentificacion.js
@@ -0,0 +1,30 @@
+var jwt = require('jsonwebtoken');
+var SEED = require('../config/config').SEED;
+
+// =====================================
+// Verificar Token
+// =====================================
+exports.verificaToken = function(req, resp, next) {
+    var token = req.query.token;
+
+    jwt.verify(token, SEED, (err, decoded) => {
+        if (err) {
+            return resp.status(401).json({
+                ok: false,
+                mensaje: 'Token incorrecto',
+                errors: err
+
+            });
+
+        }
+        req.usuario = decoded.usuario;
+        next();
+        // resp.status(200).json({
+        //     ok: true,
+        //     decoded: decoded
+
+        // });
+
+
+    });
+}
